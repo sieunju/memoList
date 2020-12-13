a8e318f56bf81e519fbad18289c32c527f00b46a
Sieun Ju <j.sieun73@gmail.com>
2020-12-13 11:33:20 +0900

    더미 데이터 추가..

---
+++ server/utils/dummyUtil.js
@@ -0,0 +1,327 @@
+
+exports.dummyMainTestJson = function () {
+    return {
+      "status": true,
+      "errMsg": "",
+      "headerContents": [
+        {
+          "v_image_path": "resource/img/IMG_1594645069528uibrhjcizvf.jpeg",
+          "v_url": "https://www.naver.com",
+          "v_code": "dummyCode1",
+          "v_title": "더미 이미지"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1594645115336nywr85bdph.jpeg",
+          "v_url": "https://www.google.com",
+          "v_code": "dummyCode2",
+          "v_title": "클래식"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1595895721787p0rnmvea02i.jpeg",
+          "v_url": "https://www.daum.net",
+          "v_code": "dummyCode3",
+          "v_title": "클래식 좋다~"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1595897676051afk1j1b40cc.jpeg",
+          "v_url": "https://www.apple.com/kr/",
+          "v_code": "appleCode",
+          "v_title": "애플코리아"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1597123638779byf0jrtatx.jpeg",
+          "v_url": "https://github.com/sieunju",
+          "v_code": "github",
+          "v_title": "깃허브!"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1597844095975j679mbp3mjg.png",
+          "v_url": "https://romannurik.github.io/AndroidAssetStudio/index.html",
+          "v_code": "androidIcon",
+          "v_title": "안드로이드 아이콘"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1597844367408f3u70agcqhv.png",
+          "v_url": "https://poiemaweb.com/",
+          "v_code": "webDevelop",
+          "v_title": "웹 프로그래밍"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1600269520224wv2my6fswzf.png",
+          "v_url": "https://dict.naver.com/",
+          "v_code": "dictNaver",
+          "v_title": "네이버 사전"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1600269572389jy005eyu8n.png",
+          "v_url": "https://en.dict.naver.com/#/main",
+          "v_code": "eglish",
+          "v_title": "하이룽"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1600269572397g9a59t9gkeq.png",
+          "v_url": "https://www.youtube.com",
+          "v_code": "youtube",
+          "v_title": "유튜브~"
+        },
+        {
+          "v_image_path": "resource/img/IMG_1600269573459y3emo46dooq.png",
+          "v_url": "https://///",
+          "v_code": "trick..",
+          "v_title": "여긴 트릭"
+        },
+        {
+          "v_image_path": "IMG_1600269573463r6n7c3n8xd.png",
+          "v_url": "https://docs.google.com/spreadsheets/u/0/",
+          "v_code": "홋홋",
+          "v_title": "하이룽ㅇㅇ"
+        }
+      ],
+      "mainContents": [
+        {
+          "bannerList": [
+            {
+              "manage_no": 9,
+              "v_image_path": "resource/img/IMG_1600269754663qqfohtruvia.png"
+            },
+            {
+              "manage_no": 10,
+              "v_image_path": "resource/img/IMG_1600269800359maeb4ak2d8.png"
+            },
+            {
+              "manage_no": 11,
+              "v_image_path": "resource/img/IMG_1600269800363i6ep6nasvr.png"
+            }
+          ],
+          "v_card_type": "A0001",
+          "v_card_name": "배너 첫번째"
+        },
+        {
+          "bannerlist": [
+            {
+              "manage_no": 62,
+              "v_img_url": "/resource/img/IMG_1600270165215k4nwgyav04.png",
+              "v_url": "https://www.naver.com"
+            },
+            {
+              "manage_no": 63,
+              "v_img_url": "/resource/img/IMG_1600270165221zxk840g0i2.png",
+              "v_url": "https://www.naver.com"
+            },
+            {
+              "manage_no": 64,
+              "v_img_url": "/resource/img/IMG_1600270165215k4nwgyav04.png",
+              "v_url": "https://www.naver.com"
+            },
+            {
+              "manage_no": 65,
+              "v_img_url": "/resource/img/IMG_1600270165215k4nwgyav04.png",
+              "v_url": "https://www.naver.com"
+            },
+            {
+              "manage_no": 66,
+              "v_img_url": "/resource/img/IMG_1600270165215k4nwgyav04.png",
+              "v_url": "https://www.naver.com"
+            },
+            {
+              "manage_no": 67,
+              "v_img_url": "/resource/img/IMG_1600270165215k4nwgyav04.png",
+              "v_url": "https://www.naver.com"
+            },
+            {
+              "manage_no": 68,
+              "v_img_url": "/resource/img/IMG_1600270165215k4nwgyav04.png",
+              "v_url": "https://www.naver.com"
+            }
+          ],
+          "v_card_type": "A0002",
+          "v_content": "언넝 사세요~UpTo 50%",
+          "v_title": "특가 세일"
+        },
+        {
+          "v_card_type": "A0002",
+          "v_sub_card_type": "B0001",
+          "v_content": "언넝 사세요-UpTo 80%",
+          "events": [
+            {
+              "v_event_reg_dt": "20201216",
+              "v_event_en_dt": "20201230",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202011/IMG1605rWV257707592.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "Y",
+              "type": "E",
+              "n_num": 1,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202011/IMG1605pCb257712740.jpg?quality=80",
+              "v_eventcd": "EVT2011TODAY",
+              "v_eventnm": "오늘도착, 전국지역 서비스 OPEN🚀"
+            },
+            {
+              "v_event_st_dt": "20201028",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202010/IMG1603ESq787534911.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 2,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1603ojx787550099.jpg?quality=80",
+              "v_eventcd": "EVT2010SAMPLE",
+              "v_eventnm": "✨샘플마켓 시즌2, 샘플링 체험단 모집✨"
+            },
+            {
+              "v_event_st_dt": "20201102",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 3,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1604ilW118119654.jpg?quality=80",
+              "v_eventcd": "EVT2011FTLIVETV",
+              "v_eventnm": "매일매일 LIVE되는 아리따움 TV😍"
+            },
+            {
+              "v_event_st_dt": "20201101",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202010/IMG1603gJt958280499.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 4,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1603wyV958288294.jpg?quality=80",
+              "v_eventcd": "EVT2011APP1BUY",
+              "v_eventnm": "2020 아리따움 App 첫 구매 혜택"
+            },
+            {
+              "v_event_st_dt": "20201101",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202010/IMG1603lmr958807230.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 5,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1603lnA958816475.jpg?quality=80",
+              "v_eventcd": "EVT2011GIFT",
+              "v_eventnm": "아리따움 온라인몰 ✨11월 구매증정혜택✨"
+            },
+            {
+              "v_event_st_dt": "20201104",
+              "v_event_en_dt": "20201125",
+              "v_remaining_dtm2": "811636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202011/IMG1604xum278978831.jpg?quality=80",
+              "v_remaining_dtm": "811636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 6,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202011/IMG1604Cvn278986664.jpg?quality=80",
+              "v_eventcd": "EVT2011REWARD",
+              "v_eventnm": "11월의 수요일도 아리따움몰 RUN👣"
+            }
+          ],
+          "v_title": "이벤트"
+        },
+        {
+          "v_card_type": "A0003",
+          "v_sub_card_type": "B0001",
+          "v_content": "아 귀찮다...더미데이터",
+          "personalList": [
+            {
+              "v_event_reg_dt": "20201216",
+              "v_event_en_dt": "20201230",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202011/IMG1605rWV257707592.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "Y",
+              "type": "E",
+              "n_num": 1,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202011/IMG1605pCb257712740.jpg?quality=80",
+              "v_eventcd": "EVT2011TODAY",
+              "v_eventnm": "오늘도착, 전국지역 서비스 OPEN🚀"
+            },
+            {
+              "v_event_st_dt": "20201028",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202010/IMG1603ESq787534911.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 2,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1603ojx787550099.jpg?quality=80",
+              "v_eventcd": "EVT2010SAMPLE",
+              "v_eventnm": "✨샘플마켓 시즌2, 샘플링 체험단 모집✨"
+            },
+            {
+              "v_event_st_dt": "20201102",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 3,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1604ilW118119654.jpg?quality=80",
+              "v_eventcd": "EVT2011FTLIVETV",
+              "v_eventnm": "매일매일 LIVE되는 아리따움 TV😍"
+            },
+            {
+              "v_event_st_dt": "20201101",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202010/IMG1603gJt958280499.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 4,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1603wyV958288294.jpg?quality=80",
+              "v_eventcd": "EVT2011APP1BUY",
+              "v_eventnm": "2020 아리따움 App 첫 구매 혜택"
+            },
+            {
+              "v_event_st_dt": "20201101",
+              "v_event_en_dt": "20201130",
+              "v_remaining_dtm2": "1243636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202010/IMG1603lmr958807230.jpg?quality=80",
+              "v_remaining_dtm": "1243636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 5,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202010/IMG1603lnA958816475.jpg?quality=80",
+              "v_eventcd": "EVT2011GIFT",
+              "v_eventnm": "아리따움 온라인몰 ✨11월 구매증정혜택✨"
+            },
+            {
+              "v_event_st_dt": "20201104",
+              "v_event_en_dt": "20201125",
+              "v_remaining_dtm2": "811636485",
+              "v_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A030/202011/IMG1604xum278978831.jpg?quality=80",
+              "v_remaining_dtm": "811636485",
+              "v_flag_event_category": "N",
+              "type": "E",
+              "n_num": 6,
+              "v_mobile_banner_image_path": "https://qtzz.synology.me:1010/UPLOAD/UPLOAD_IMAGE/A031/202011/IMG1604Cvn278986664.jpg?quality=80",
+              "v_eventcd": "EVT2011REWARD",
+              "v_eventnm": "11월의 수요일도 아리따움몰 RUN👣"
+            }
+          ],
+          "v_title": "이벤트"
+        },
+        {
+          "v_card_type": "A0004",
+          "v_sub_card_type": "B0002",
+          "v_content": "이미지 하나짜리",
+          "v_title": "이미지 하나짜리 타입",
+          "v_image_path": "/resource/img/IMG_1600295496493jprn21hc2va.png"
+        },
+        {
+          "v_card_type": "A0005",
+          "v_sub_card_type": "B0003",
+          "v_content": "이미지 하나짜리",
+          "v_title": "이미지 이건 둥글게",
+          "v_image_path": "/resource/img/IMG_1600600893607k1mbe7y0rr.png"
+        }
+      ]
+    }
+}
