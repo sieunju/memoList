7a630b3a82788195305ec140a0c1a6c2db209413
hmju <office_hmju>
2021-01-22 16:10:01 +0900

    - TEST

--- client/public/js/daum_address_return.js
+++ client/public/js/daum_address_return.js
@@ -7,13 +7,12 @@ var element_layer = document.getElementById('layer');
                                   // userSelectedType : 검색 결과에서 사용자가 선택한 주소의 타입
                                   // return type : R - roadAddress, J : jibunAddress
                                   // TestApp 은 안드로이드에서 등록한 이름
-                                  alert("되었나?1")
-                                  window.TestApp.setAddress(data.zonecode, data.roadAddress, data.buildingName);
+                                  alert("되었나?1");
+                                  window.OneaOneJavaInterface.setAddress(data.zonecode, data.roadAddress, data.buildingName);
                               }
                               else{
-                                  alert("되었나?2")
-                                  window.TestApp.setAddress(data.zonecode, data.jibunAddress, data.buildingName);
-
+                                  alert("되었나?2");
+                                  window.OneaOneJavaInterface.setAddress(data.zonecode, data.jibunAddress, data.buildingName);
                               }
               }
           }).embed();
